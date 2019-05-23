48f2fd870b3a717af391bcafc073bdf500b18812
0Franky <frhaexin@gmail.com>
2019-05-23 10:22:20 +0200

    Migliorata classe ConversionParam

--- WPF C#/Emotions Contest/Classes/CSV/Converter/ConversionParam.cs
+++ WPF C#/Emotions Contest/Classes/CSV/Converter/ConversionParam.cs
@@ -6,22 +6,24 @@ using System.Threading.Tasks;
 
 namespace Emotions_Contest
 {
-    class ConversionParamToArrays
+    class ConversionParam
     {
-        //public static List<string> convert(string activity, int pleasantness, int excitement, string notes = "")
+        //public static List<string> convertToArray(DateTime startDate, string activity, int pleasantness, int excitement, string notes, DateTime endDate)
         //{
         //    List<string> listParam = new List<string>();
 
+        //    listParam.Add(startDate.ToString("yyyy/MM/dd HH:mm:ss"));
         //    listParam.Add(activity);
         //    listParam.Add(pleasantness.ToString());
         //    listParam.Add(excitement.ToString());
         //    listParam.Add(notes);
+        //    listParam.Add(endDate.ToString("yyyy/MM/dd HH:mm:ss"));
 
         //    return listParam;
         //}
 
 
-        public static List<string> convert(DateTime startDate, string activity, string pleasantness, string excitement, string notes, DateTime endDate)
+        public static List<string> convertToArray(DateTime startDate, string activity, string pleasantness, string excitement, string notes, DateTime endDate)
         {
             List<string> listParam = new List<string>();
 
