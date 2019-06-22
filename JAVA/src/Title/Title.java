520449a7d21956b80b9884827af507db174bb927
0Franky <frhaexin@gmail.com>
2019-06-22 15:52:56 +0200

    Agguirnate costanti in Title

--- JAVA/src/Title/Title.java
+++ JAVA/src/Title/Title.java
@@ -8,20 +8,25 @@ public class Title {
 	/**
 	 * Makes dynamic the NAME of the App
 	 */
-	public static String APPLICATION_NAME = "HowAppYou";
+	public static final String APPLICATION_NAME = "HowAppYou";
 
 	/**
 	 * Makes dynamic the VERSION of the App
 	 */
-	public static String APPLICATION_VERSION = "1.0";
+	public static final String APPLICATION_VERSION = "1.0";
 
 	/**
 	 * json URL online file whit credentials
 	 */
-	public static String JSON_CREDENTIAL_URL = "http://extremisinfo.altervista.org/service_account.json";
+	public static final String JSON_CREDENTIAL_URL = "http://extremisinfo.altervista.org/service_account.json";
 
 	/**
 	 * E-mails to send the spid
 	 */
-	public static String[] emails = { "f.scarati2@studenti.uniba.it", "christian-miccolis@libero.it" };
+	public static final String[] EMAILS = { "f.scarati2@studenti.uniba.it", "christian-miccolis@libero.it" };
+
+	/**
+	 * User-id
+	 */
+	public static final String USER_ID = System.getProperty("user.name");
 }
