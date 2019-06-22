149cfdba21643b9c4edb3daec4629e871b413821
0Franky <frhaexin@gmail.com>
2019-06-22 20:29:01 +0200

    MailSender fixed

--- JAVA/src/classes/MailSender.java
+++ JAVA/src/classes/MailSender.java
@@ -4,11 +4,13 @@ import java.util.Properties;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
+import Title.Title;
 
 /**
  * Useful classe to send emails
@@ -24,83 +26,50 @@ public class MailSender {
 	 * @param sender
 	 * @param password
 	 * @param reciver
-	 * @param oggetto
-	 * @param content
+	 * @param subject
+	 * @param body
 	 */
-	public static void sendMail(String sender, String password, String receiver, String oggetto, String content) {
+	public static void sendMail(String sender, String password, String receiver, String subject, String body) {
 
-		String testo = ""; // Non necessario se si usa il content html in input //
+		String host = "smtp.gmail.com";
 
 		Properties props = new Properties();
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.ssl.trust", host);
 		props.put("mail.smtp.port", "587");
 
-		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(sender, password);
-			}
-		});
+		Session session = Session.getDefaultInstance(props);
+		MimeMessage message = new MimeMessage(session);
 
 		try {
 
-			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress(sender));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiver));
-			message.setSubject(oggetto);
-			message.setText(testo);
-			message.setContent(content, "text/html");
+			InternetAddress toAddress = new InternetAddress(receiver);
 
-			Transport.send(message);
+			message.addRecipient(Message.RecipientType.TO, toAddress);
+
+			message.setSubject(subject);
+			message.setText(body);
+
+			Transport transport = session.getTransport("smtp");
+
+			transport.connect(host, sender, password);
+			transport.sendMessage(message, message.getAllRecipients());
+			transport.close();
 
 			System.out.println("Done");
 
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
+		} catch (AddressException ae) {
+			ae.printStackTrace();
+		} catch (MessagingException me) {
+			me.printStackTrace();
 		}
 	}
 
 	// MAIN TEST //
 	public static void main(String[] args) {
-
-		final String sender = "howappyou@gmail.com";
-		final String password = "Qwerty#123";
-
-		final String receiver = "c.miccolis3@studenti.uniba.it";
-
-		String oggetto = " Oggetto ";
-		String testo = "";
-
-		String content = "<h1> Intestazione H1 </h1>" + "<p> paragrafo 1 </p>" + "<p> paragrafo 2 </p>";
-
-		Properties props = new Properties();
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
-
-		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(sender, password);
-			}
-		});
-
-		try {
-
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(sender));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiver));
-			message.setSubject(oggetto);
-			message.setText(testo);
-			message.setContent(content, "text/html");
-
-			Transport.send(message);
-
-			System.out.println("Done");
-
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
+		MailSender.sendMail(Title.EMAILS_SENDER, Title.PASSWORD_EMAILS_SENDER, "f.scarati2@studenti.uniba.it",
+				"00SPID test", "myspid00");
 	}
 }
